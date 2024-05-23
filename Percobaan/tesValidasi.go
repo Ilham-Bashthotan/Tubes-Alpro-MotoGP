61f69c01e5de2b3ac2ff945fe5c8162f9b903ecc
ilham-bashthotan <ilhambashthotan123@gmail.com>
2024-05-23 23:15:43 +0700

    perbaikan error

--- Percobaan/tesValidasi.go
+++ Percobaan/tesValidasi.go
@@ -6,7 +6,7 @@ import (
 	"os/exec"
 )
 
-const NMAX = 100
+const NMAX = 20
 
 type produk struct {
 	barang string
@@ -18,24 +18,25 @@ type tabProduk [NMAX]produk
 func main() {
 	var dataProduk tabProduk
 	var nDataProduk int
-	inputProduk(&dataProduk, nDataProduk)
+	inputProduk(&dataProduk, &nDataProduk)
+	cetakProduk(dataProduk, nDataProduk)
 }
 
-func inputProduk(T *tabProduk, n int) {
+func inputProduk(T *tabProduk, n *int) {
 	clear_screen()
 	fmt.Print("masukkan banyak produk yang di input (1-20): ")
 	for {
-		fmt.Scan(&n)
-		if !(1 <= n && n <= NMAX) {
+		fmt.Scan(n)
+		if !(1 <= *n && *n <= NMAX) {
 			clear_screen()
 			fmt.Println("Masukan tidak valid")
 			fmt.Print("masukkan banyak produk yang di input (1-20): ")
 		}
-		if 1 <= n && n <= NMAX {
+		if 1 <= *n && *n <= NMAX {
 			break
 		}
 	}
-	for i := 0; i < n; i++ {
+	for i := 0; i < *n; i++ {
 		fmt.Print("Data ke-", i + 1, "\n")
 		fmt.Print("masukkan nama: ")
 		inputNama(&T[i].barang)
@@ -45,8 +46,11 @@ func inputProduk(T *tabProduk, n int) {
 }
 
 func cetakProduk(T tabProduk, n int) {
+	clear_screen()
+	fmt.Println("Isi Produk: ")
+	// fmt.Println(T)
 	for i := 0; i < n; i++ {
-		fmt.Println(T[i].barang, T[i].harga)
+		fmt.Println(i+1, T[i].barang, T[i].harga)
 	}
 }
 
