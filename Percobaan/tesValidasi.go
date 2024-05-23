07a48429dfc76d4ea786968462ba1a290c767811
ilham-bashthotan <ilhambashthotan123@gmail.com>
2024-05-23 22:47:44 +0700

    contoh Validasi

---
+++ Percobaan/tesValidasi.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const NMAX = 100
+
+type produk struct {
+	barang string
+	harga  int
+}
+
+type tabProduk [NMAX]produk
+
+func main() {
+	var dataProduk tabProduk
+	var nDataProduk int
+	inputProduk(&dataProduk, nDataProduk)
+}
+
+func inputProduk(T *tabProduk, n int) {
+	clear_screen()
+	fmt.Print("masukkan banyak produk yang di input (1-20): ")
+	for {
+		fmt.Scan(&n)
+		if !(1 <= n && n <= NMAX) {
+			clear_screen()
+			fmt.Println("Masukan tidak valid")
+			fmt.Print("masukkan banyak produk yang di input (1-20): ")
+		}
+		if 1 <= n && n <= NMAX {
+			break
+		}
+	}
+	for i := 0; i < n; i++ {
+		fmt.Print("Data ke-", i + 1, "\n")
+		fmt.Print("masukkan nama: ")
+		inputNama(&T[i].barang)
+		fmt.Print("masukkan harga: ")
+		fmt.Scan(&T[i].harga)
+	}
+}
+
+func cetakProduk(T tabProduk, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Println(T[i].barang, T[i].harga)
+	}
+}
+
+func inputNama(str *string) {
+	var ch, i byte
+
+	*str = ""
+	for i = 1; i <= 2; i++ {
+		fmt.Scanf("%c", &ch)
+		if ch != '\r' && ch != '\n' {
+			*str += string(ch)
+		}
+	}
+	for {
+		fmt.Scanf("%c", &ch)
+		if ch != '\r' && ch != '\n' {
+			*str += string(ch)
+		}
+		if ch == '\n' {
+			break
+		}
+	}
+}
+
+func clear_screen() {
+	c := exec.Command("cmd", "/c", "cls")
+	c.Stdout = os.Stdout
+	c.Run()
+}
