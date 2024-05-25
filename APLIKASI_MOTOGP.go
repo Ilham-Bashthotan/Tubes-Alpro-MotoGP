5ef549511ba3730d2d17a4e498d61e7cf8473875
ilham-bashthotan <ilhambashthotan123@gmail.com>
2024-05-25 18:08:02 +0700

    perbaikan ui

--- APLIKASI_MOTOGP.go
+++ APLIKASI_MOTOGP.go
@@ -6,47 +6,43 @@ import (
 	"os/exec"
 )
 
-type rider struct {
-	name, no, nat, team                                                 string
-    timeFP1, timePR, timeFP2, timeQ1, timeQ2, timeSPR, timeWUP, timeRAC int
-    poinSPR, poinRAC, totalPoin                                         int
-}
-
-type subRider struct {
-	name, no, nat, team, timeGap string
-    time                         int
-}
-
 const NMAX int = 24
 
-type tabRider [NMAX]rider
+type time struct {
+	no, time int
+}
 
-type tabSubRider [NMAX]subRider
+type tabTime [NMAX]time
 
+type riders struct {
+	no                                  [NMAX]int
+	name, nat, team                     [NMAX]string
+    FP1, PR, FP2, Q1, Q2, SPR, WUP, RAC tabTime
+}
 
-var dataRider tabRider
+var dataRider riders
 var nDataRider int
-var dataFP1, dataPR, dataFP2, dataQ1, dataQ2, dataSPR, dataWUP, dataRAC tabSubRider
+var dataFP1, dataPR, dataFP2, dataQ1, dataQ2, dataSPR, dataWUP, dataRAC tabTime
 
 func main() {
-	var pilih int
+	var pilih string
 
 	for {
 		intro()
 		menuUtama(&pilih)
 		switch pilih {
-		case 1: 
+		case "1": 
 			inputDataRider()
-		case 2: 
+		case "2": 
 			tampilkanDataRider()
-		case 3: 
+		case "3": 
 			editDataRider()
-		case 4: 
+		case "4": 
 			hapusDataRider()
 		default:
 			clear_screen()
 		} 
-		if pilih == 5 {
+		if pilih == "5" {
 			break
 		}
 	}
@@ -72,7 +68,7 @@ func bye() {
     fmt.Println("Faiz Nawfal Zulfiqar")
 }
 
-func menuUtama(p *int) {
+func menuUtama(p *string) {
 	fmt.Println("--------------------------")
 	fmt.Println("          M E N U         ")
 	fmt.Println("--------------------------")
@@ -83,10 +79,10 @@ func menuUtama(p *int) {
 	fmt.Println("5. Keluar")
 	fmt.Println("--------------------------")
 	fmt.Print("Pilih (1-5): ")
-	fmt.Scan(p)
+	inputFrasa(p)
 }
 
-func menuInputDataRider(p *int) {
+func menuInputDataRider(p *string) {
 	fmt.Println("--------------------------")
 	fmt.Println("     Input Data Rider     ")
 	fmt.Println("--------------------------")
@@ -102,10 +98,10 @@ func menuInputDataRider(p *int) {
 	fmt.Println("10. Kembali")
 	fmt.Println("--------------------------")
 	fmt.Print("Pilih (1-10): ")
-	fmt.Scan(p)
+	inputFrasa(p)
 }
 
-func menuTampilkanDataRider(p *int) {
+func menuTampilkanDataRider(p *string) {
 	fmt.Println("--------------------------")
 	fmt.Println("   Tampilkan Data Rider   ")
 	fmt.Println("--------------------------")
@@ -122,10 +118,10 @@ func menuTampilkanDataRider(p *int) {
 	fmt.Println("11. Kembali")
 	fmt.Println("--------------------------")
 	fmt.Print("Pilih (1-11): ")
-	fmt.Scan(p)
+	inputFrasa(p)
 }
 
-func menuEditDataRider(p *int) {
+func menuEditDataRider(p *string) {
 	fmt.Println("--------------------------")
 	fmt.Println("     Edit Data Rider      ")
 	fmt.Println("--------------------------")
@@ -141,10 +137,10 @@ func menuEditDataRider(p *int) {
 	fmt.Println("10. Kembali")
 	fmt.Println("--------------------------")
 	fmt.Print("Pilih (1-10): ")
-	fmt.Scan(p)
+	inputFrasa(p)
 }
 
-func menuHapusDataRider(p *int) {
+func menuHapusDataRider(p *string) {
 	fmt.Println("--------------------------")
 	fmt.Println("     Hapus Data Rider     ")
 	fmt.Println("--------------------------")
@@ -153,28 +149,28 @@ func menuHapusDataRider(p *int) {
 	fmt.Println("3. Kembali")
 	fmt.Println("--------------------------")
 	fmt.Print("Pilih (1/2/3): ")
-	fmt.Scan(p)
+	inputFrasa(p)
 }
 
 func inputDataRider() {
-	var pilih int
+	var pilih string
 
 	clear_screen()
 	for {
 		menuInputDataRider(&pilih)
 		switch pilih {
-			case 1:	inputRider(&dataRider, nDataRider)
-			case 2:	inputFP1(&dataRider, nDataRider)
-			case 3:	inputPR(&dataRider, nDataRider)
-			case 4:	inputFP2(&dataRider, nDataRider)
-			case 5:	inputQ1(&dataRider, nDataRider)
-			case 6:	inputQ2(&dataRider, nDataRider)
-			case 7:	inputSPR(&dataRider, nDataRider)
-			case 8:	inputWUP(&dataRider, nDataRider)
-			case 9:	inputRAC(&dataRider, nDataRider)
+			// case "1":	inputRider(&dataRider, nDataRider)
+			// case "2":	inputFP1(&dataRider, nDataRider)
+			// case "3":	inputPR(&dataRider, nDataRider)
+			// case "4":	inputFP2(&dataRider, nDataRider)
+			// case "5":	inputQ1(&dataRider, nDataRider)
+			// case "6":	inputQ2(&dataRider, nDataRider)
+			// case "7":	inputSPR(&dataRider, nDataRider)
+			// case "8":	inputWUP(&dataRider, nDataRider)
+			// case "9":	inputRAC(&dataRider, nDataRider)
 			default: clear_screen()
 		}
-		if pilih == 10 {
+		if pilih == "10" {
 			clear_screen()
 			break
 		}
@@ -182,25 +178,25 @@ func inputDataRider() {
 }
 
 func tampilkanDataRider() {
-	var pilih int
+	var pilih string
 
 	clear_screen()
 	for {
 		menuTampilkanDataRider(&pilih)
 		switch pilih {
-			case 1:	cetakTabelPemain(dataRider, nDataRider)
-			case 2:	cetakTabelWaktu(dataFP1, nDataRider)
-			case 3:	cetakTabelWaktu(dataPR, nDataRider)
-			case 4:	cetakTabelWaktu(dataFP2, nDataRider)
-			case 5:	cetakTabelWaktu(dataQ1, nDataRider)
-			case 6:	cetakTabelWaktu(dataQ2, nDataRider)
-			case 7:	cetakTabelWaktu(dataSPR, nDataRider)
-			case 8:	cetakTabelWaktu(dataWUP, nDataRider)
-			case 9:	cetakTabelWaktu(dataRAC, nDataRider)
-			case 10: cetakTabelPoin(dataRider, nDataRider)
+			// case "1":	cetakTabelPemain(dataRider, nDataRider)
+			// case "2":	cetakTabelWaktu(dataFP1, nDataRider)
+			// case "3":	cetakTabelWaktu(dataPR, nDataRider)
+			// case "4":	cetakTabelWaktu(dataFP2, nDataRider)
+			// case "5":	cetakTabelWaktu(dataQ1, nDataRider)
+			// case "6":	cetakTabelWaktu(dataQ2, nDataRider)
+			// case "7":	cetakTabelWaktu(dataSPR, nDataRider)
+			// case "8":	cetakTabelWaktu(dataWUP, nDataRider)
+			// case "9":	cetakTabelWaktu(dataRAC, nDataRider)
+			// case "10": cetakTabelPoin(dataRider, nDataRider)
 			default: clear_screen()
 		}
-		if pilih == 11 {
+		if pilih == "11" {
 			clear_screen()
 			break
 		}
@@ -208,24 +204,24 @@ func tampilkanDataRider() {
 }
 
 func editDataRider() {
-	var pilih int
+	var pilih string
 
 	clear_screen()
 	for {
 		menuEditDataRider(&pilih)
 		switch pilih {
-			case 1: 
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
-			case 9:
+			case "1": 
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
 			default: clear_screen()
 		}
-		if pilih == 10 {
+		if pilih == "10" {
 			clear_screen()
 			break
 		}
@@ -233,7 +229,7 @@ func editDataRider() {
 }
 
 func hapusDataRider() {
-	var pilih int
+	var pilih string
 /*
 	fmt.Println("1. Rider")
 	fmt.Println("2. Semua Rider")
@@ -243,11 +239,11 @@ func hapusDataRider() {
 	for {
 		menuHapusDataRider(&pilih)
 		switch pilih {
-			case 1: 
-			case 2:
+			case "1": 
+			case "2":
 			default: clear_screen()
 		}
-		if pilih == 3 {
+		if pilih == "3" {
 			clear_screen()
 			break
 		}
@@ -262,76 +258,76 @@ func hapusDataRider() {
 // 	return
 // }
 
-func hitungTotalPoin(S rider) int {
-/*mengembalikan nilai total poin S rider*/
-    return S.poinSPR + S.poinRAC
-}
+// func hitungTotalPoin(S rider) int {
+// /*mengembalikan nilai total poin S rider*/
+//     return S.poinSPR + S.poinRAC
+// }
 
 
 
-func inputRider(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai. Banyak elemen (n) berisi nilai NMAX, jika n > NMAX*/
-} 
+// func inputRider(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai. Banyak elemen (n) berisi nilai NMAX, jika n > NMAX*/
+// } 
 
-func inputFP1(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai memasukkan field timeFP1*/
-}
+// func inputFP1(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai memasukkan field timeFP1*/
+// }
 
-func inputPR(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai memasukkan field timePR*/
-}
+// func inputPR(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai memasukkan field timePR*/
+// }
 
-func inputFP2(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai memasukkan field timeFP2*/
-}
+// func inputFP2(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai memasukkan field timeFP2*/
+// }
 
-func inputQ1(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai memasukkan field timeQ1*/
-}
+// func inputQ1(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai memasukkan field timeQ1*/
+// }
 
-func inputQ2(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai memasukkan field timeQ2*/
-}
+// func inputQ2(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai memasukkan field timeQ2*/
+// }
 
-func inputSPR(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai memasukkan field timeSPR*/
-}
+// func inputSPR(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai memasukkan field timeSPR*/
+// }
 
-func inputWUP(T *tabRider, n int) {
-	/*I.S. terdefinisi array T sebanyak n
-	F.S. T berisi nilai memasukkan field timeWUP*/
-	}
+// func inputWUP(T *tabRider, n int) {
+// 	/*I.S. terdefinisi array T sebanyak n
+// 	F.S. T berisi nilai memasukkan field timeWUP*/
+// 	}
 
-func inputRAC(T *tabRider, n int) {
-/*I.S. terdefinisi array T sebanyak n
-F.S. T berisi nilai memasukkan field timeRAC*/
-}
+// func inputRAC(T *tabRider, n int) {
+// /*I.S. terdefinisi array T sebanyak n
+// F.S. T berisi nilai memasukkan field timeRAC*/
+// }
 
-func urutWaktu(T *tabSubRider) {
-/*I.S. terdefinisi array acak T1 sebanyak n
-F.S. T berisi nilai terurut berdasarkan waktu*/
-	var i, minPos, pass int
-	var temp subRider
+// func urutWaktu(T *tabSubRider) {
+// /*I.S. terdefinisi array acak T1 sebanyak n
+// F.S. T berisi nilai terurut berdasarkan waktu*/
+// 	var i, minPos, pass int
+// 	var temp subRider
 
-	for pass = 1; pass < 6; pass++ {
-		minPos = pass - 1
-		for i = pass; i < 6; i++ {
-			if T[i].time < T[minPos].time {
-				minPos = i
-			}
-		}
-		temp = T[minPos]
-		T[minPos] = T[pass - 1]
-		T[pass - 1] = temp
-	}
-}
+// 	for pass = 1; pass < 6; pass++ {
+// 		minPos = pass - 1
+// 		for i = pass; i < 6; i++ {
+// 			if T[i].time < T[minPos].time {
+// 				minPos = i
+// 			}
+// 		}
+// 		temp = T[minPos]
+// 		T[minPos] = T[pass - 1]
+// 		T[pass - 1] = temp
+// 	}
+// }
 
 func inputFrasa(str *string) {
 /*I.S str 
@@ -363,19 +359,19 @@ func clear_screen() {
 	c.Run()
 }
 
-func cetakTabelPemain(T tabRider, n int) {
-/*I.S. T array dan banyak elemen n terdefinisi
-F.S. tercetak tabel dari field : name, no, nat, team*/
+// func cetakTabelPemain(T tabRider, n int) {
+// /*I.S. T array dan banyak elemen n terdefinisi
+// F.S. tercetak tabel dari field : name, no, nat, team*/
 
-}
+// }
 
-func cetakTabelWaktu(T tabSubRider, n int) {
-/*I.S. T array dan banyak elemen n terdefinisi
-F.S. tercetak tabel dari field : name, no, nat, team, time*/
+// func cetakTabelWaktu(T tabSubRider, n int) {
+// /*I.S. T array dan banyak elemen n terdefinisi
+// F.S. tercetak tabel dari field : name, no, nat, team, time*/
 
-}
+// }
 
-func cetakTabelPoin(T tabRider, n int) {
-/*I.S T array dan banyak elemen n terdefinisi
-	F.S tercetak tabel dari field : name, no, nat, team, totalPoin*/
-}
+// func cetakTabelPoin(T tabRider, n int) {
+// /*I.S T array dan banyak elemen n terdefinisi
+// 	F.S tercetak tabel dari field : name, no, nat, team, totalPoin*/
+// }
